223c3da8dedb3b12401546da0b2614d30bd8f899
RVNJHN08 <ajsuyat8@gmail.com>
2024-12-16 00:50:32 +0800

    fixing functionality

--- tryGit/index.js
+++ tryGit/index.js
@@ -1,10 +1,26 @@
 let username;
 
 document.getElementById("submitNow").onclick = function () {
-  username = document.getElementById("UserName").value;
+  username = document.getElementById("userName").value;
   if (username) {
-    document.getElementById("userGreetings").textContent = `Hello, ${username}`;
+    document.getElementById(
+      "userGreetings"
+    ).textContent = `Hello, ${username} !`;
+    document.getElementById("userName").value = "";
+    document.getElementById("removeNow").style.display = "inline";
+    document.getElementById("submitNow").style.display = "none";
   } else {
-    document.getElementById("userGreetings").textContent = `wew`;
+    document.getElementById(
+      "userGreetings"
+    ).textContent = `Hello, Please write your username`;
+    document.getElementById;
   }
+};
+
+document.getElementById("removeNow").onclick = function () {
+  document.getElementById("userGreetings").textContent =
+    "Hello, Please write your username";
+  document.getElementById("userName").value = "";
+  document.getElementById("removeNow").style.display = "none";
+  document.getElementById("submitNow").style.display = "inline";
 };
