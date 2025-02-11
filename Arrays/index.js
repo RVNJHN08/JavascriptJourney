8f6fc0161eaafe72e180fcb8e39d9f1aec354505
RVNJHN08 <ajsuyat8@gmail.com>
2025-02-11 13:32:46 +0800

    About Tech Field Assistance

---
+++ Arrays/index.js
@@ -0,0 +1,24 @@
+//display = document.getElementById("display");
+//let fruit = ["apple", "banana", "grapes", "mango", "orange"];
+//fruit.unshift("kiwi");
+//fruit.push("pear");
+//fruit.shift();
+//fruit.pop();
+//fruit.splice(2, "", "lemon");
+//display.textContent = fruit;
+
+//display.textContent = hello(goodbye);
+//hello(display);
+//goodbye();
+//function hello(callback) {
+//let message = "Hello World!";
+//callback(message);
+//}
+
+//function goodbye() {
+// return "Goodbye World!";
+//}
+
+//function display(message) {
+// document.getElementById("display").textContent = message;
+//}
